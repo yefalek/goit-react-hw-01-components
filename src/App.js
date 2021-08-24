64984c256d1f78af9afb00a7ff179ebb2a97e3a3
yefalek <yefremenkov.alek@gmail.com>
2021-08-24 16:04:53 +0300

    add code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-// import logo from './logo.svg';
-// import './App.css';
+import userData from './Profile/user.json';
+import User from './Profile/user';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-// export default App;
+function App() {
+  return (
+    <div className="App">
+      <User
+        name={userData.name}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
