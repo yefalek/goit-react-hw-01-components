f80d9436065d27bb8bb2748163b28e126eec5542
yefalek <yefremenkov.alek@gmail.com>
2021-08-27 21:04:52 +0300

    add style, task-3 ready

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import user from './Profile/user.json';
 import Profile from './Profile/profile';
 import statisticalData from './Statistics/statistical-data.json';
 import Statistics from './Statistics/statistics';
+import friends from './FriendList/friends.json';
+import FriendList from './FriendList/friendlist';
 
 
 export default function App() {
   return (
-    <div>
+    <div class = "general">
       <Profile
         name={user.name}
         tag={user.tag}
@@ -14,9 +16,12 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+
       <Statistics
         title="Upload stats" stats={statisticalData} />
       {/* <Statistics stats={statisticalData} /> */}
+
+      <FriendList friends={friends} />,
     </div>
   );
 }
---
+++ src/FriendList/friendlist.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import css from '../index.css';
+
+function FriendList({ friends }) {
+    return (
+        <div class="friends-container">
+            <ul class="friend-list">
+                {friends.map(({ id, avatar, name, isOnline }) => (
+                    
+                    <li key={id} class="friend-item">
+                        <span class={`status ${ isOnline ? 'online' : 'offline' }`}></span>
+                        <img class="friend-icon" src={avatar} alt={name} width="48" />
+                        <p class="name">{name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+    
+}
+
+
+FriendList.propTypes = {stats: PropTypes.arrayOf(
+    PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+    }),
+  ),};
+
+export default FriendList;
