02b2a6402a5458b03d9a1b2af881b225d0cb06af
yefalek <yefremenkov.alek@gmail.com>
2021-08-29 18:30:28 +0300

    fix1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import user from './Profile/user.json';
-import Profile from './Profile/profile';
-import statisticalData from './Statistics/statistical-data.json';
-import Statistics from './Statistics/statistics';
-import friends from './FriendList/friends.json';
-import FriendList from './FriendList/friendlist';
-import transactions from './TransactionHistory/transactions.json';
-import TransactionHistory from './TransactionHistory/transactions';
-
+import user from './components/Profile/user.json';
+import Profile from './components/Profile/profile';
+import statisticalData from './components/Statistics/statistical-data.json';
+import Statistics from './components/Statistics/statistics';
+import friends from './components/FriendList/friends.json';
+import FriendList from './components/FriendList/friendlist';
+import transactions from './components/TransactionHistory/transactions.json';
+import TransactionHistory from './components/TransactionHistory/transactions';
 
 export default function App() {
   return (
-    <div class = "general">
+    <div class="general">
       <Profile
         name={user.name}
         tag={user.tag}
@@ -18,15 +17,9 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-
-      <Statistics
-        title="Upload stats" stats={statisticalData} />
-      {/* <Statistics stats={statisticalData} /> */}
-
+      <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friends} />
-
       <TransactionHistory items={transactions} />
     </div>
   );
 }
-
